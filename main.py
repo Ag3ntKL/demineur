b069ff3af05078bac564ace64c396a4b8f9d431c
AgentKL <75439661+Ag3ntKL@users.noreply.github.com>
2022-12-15 23:30:43 +0100

    Delete main.py

--- main.py
+++
@@ -1,6 +0,0 @@
-from http.server import SimpleHTTPRequestHandler
-from socketserver import TCPServer
-from webbrowser import open
-
-open('http://localhost:8000/html')
-TCPServer(("", 8000), SimpleHTTPRequestHandler).serve_forever()
